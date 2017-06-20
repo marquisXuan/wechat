35e1bd75de6752c202aa8a9b2bf921c95d9381d6
YeYunXuan <ycountjavaxuan@outlook.com>
2017-06-20 10:51:06 +0800

    初始化项目

---
+++ src/test/java/org/yyx/study/WechatApplicationTests.java
@@ -0,0 +1,16 @@
+package org.yyx.study;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WechatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
