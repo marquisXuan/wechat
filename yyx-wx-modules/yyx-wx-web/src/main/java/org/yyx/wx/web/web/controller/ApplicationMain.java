b4781fff3ffca8f14bfd439c9a3177b29323ca5c
叶云轩.联想 <yuanzw1@lenovo.com>
2018-11-06 09:38:12 +0800

    Comment ApplicationMain.java

--- yyx-wx-modules/yyx-wx-web/src/main/java/org/yyx/wx/web/web/controller/ApplicationMain.java
+++ yyx-wx-modules/yyx-wx-web/src/main/java/org/yyx/wx/web/web/controller/ApplicationMain.java
@@ -177,6 +177,7 @@ public class ApplicationMain {
                 Element rootElement = document.getRootElement();
                 // 解析成BaseMessage对象
                 BaseMessageAndEventRequestAndResponse baseMessage = WxXmlAndObjectUtil.xmlToObject(rootElement, BaseMessageAndEventRequestAndResponse.class);
+                // 加载预
                 BaseMessageHandlerProxy[] baseMessageHandlerProxies = getBaseMessageHandlerProxies();
                 String handlerType = wxPublicNumConfig.getHandlerType().toUpperCase();
                 AbstractMessageHandler messageHandler;
