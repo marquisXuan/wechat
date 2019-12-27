3306f07f61e163a66c159387f61aadd7c3127ac4
叶云轩.MacBookPro <yzw562638362@icloud.com>
2019-12-27 10:22:58 +0800

    添加 authorization-wx 模块架子

---
+++ wx-authorization/src/main/java/com/cjwy/wxframework/authorization/AuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.cjwy.wxframework.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationApplication.class, args);
+	}
+
+}
---
+++ wx-authorization/src/test/java/com/cjwy/wxframework/authorization/AuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjwy.wxframework.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
