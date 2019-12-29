d22183a0595f321ba5401269e515b4285b15432f
叶云轩.MacBookPro <yzw562638362@icloud.com>
2019-12-29 15:29:48 +0800

    添加了一些方法

--- cjwy-commons/src/main/java/com/cjwy/projects/commons/business/domain/entity/BaseEntity.java
+++ cjwy-commons/src/main/java/com/cjwy/projects/commons/business/domain/entity/BaseEntity.java
@@ -4,9 +4,11 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.Version;
+import com.cjwy.projects.commons.string.utils.UtilString;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.util.StringUtils;
 
 import java.util.Date;
 
@@ -38,4 +40,28 @@ public class BaseEntity {
     private String createId;
 
     private String modifyId;
+
+    /**
+     * 初始化方法
+     */
+    public void initialization() {
+        if (StringUtils.isEmpty(id)) {
+            this.id = UtilString.randomUUID();
+        }
+        this.createId = this.id;
+        this.createTime = new Date();
+        this.version = 0L;
+        this.status = 0;
+    }
+
+    /**
+     * 修改时的模板方法
+     *
+     * @param modifyId 操作人 ID
+     */
+    public void modifyTemplate(String modifyId) {
+        this.modifyId = modifyId;
+        this.modifyTime = new Date();
+        this.version++;
+    }
 }
