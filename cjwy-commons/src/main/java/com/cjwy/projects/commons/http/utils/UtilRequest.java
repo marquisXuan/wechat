741ad85bf4eb770c00a0a0ed624aeb4b3844cbac
叶云轩.MacBookPro <yzw562638362@icloud.com>
2019-12-31 03:22:17 +0800

    添加 UtilRequest 工具类

---
+++ cjwy-commons/src/main/java/com/cjwy/projects/commons/http/utils/UtilRequest.java
@@ -0,0 +1,34 @@
+package com.cjwy.projects.commons.http.utils;
+
+import com.cjwy.projects.commons.http.exception.NoAuthorizationException;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * UtilRequest
+ * <p>
+ *
+ * @author 叶云轩 at tdg_yyx@foxmail.com
+ * @date 2019/12/31 3:04 上午
+ */
+public class UtilRequest {
+
+    private UtilRequest() {
+    }
+
+    /**
+     * 从请求头中获取认证信息的方法
+     *
+     * @param request 请求
+     * @return token
+     */
+    public static String getAuthorizationInHeader(HttpServletRequest request) {
+        String authorization = request.getHeader("Authorization");
+        if (StringUtils.isEmpty(authorization)) {
+            throw new NoAuthorizationException();
+        }
+        return authorization;
+    }
+
+}
