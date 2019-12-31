f1de31eb15319e5d643f3fd6220901d846561a1f
叶云轩.MacBookPro <yzw562638362@icloud.com>
2019-12-31 12:34:49 +0800

    修改版本->06

---
+++ wx-message/src/main/java/com/cjwy/wxframework/wxmessage/WxMessageApplication.java
@@ -0,0 +1,13 @@
+package com.cjwy.wxframework.wxmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxMessageApplication.class, args);
+    }
+
+}
---
+++ wx-message/src/test/java/com/cjwy/wxframework/wxmessage/WxMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjwy.wxframework.wxmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
